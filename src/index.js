526399f54f14057c21e0c2c3f49dad482c6c7990
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-12-12 10:18:42 +0200

    start conditions for practice

---
+++ src/index.js
@@ -0,0 +1,24 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    // Response handling
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
